108ff6f162af0e18e51664c83793924f43a9973e
Juan Bono <juanbono94@gmail.com>
2018-07-09 20:03:58 -0300

    bug fixes and single client flow working

--- apps/client/lib/offer_logic/offer_logic.ex
+++ apps/client/lib/offer_logic/offer_logic.ex
@@ -2,28 +2,37 @@ defmodule Client.OfferLogic do
   def process(body_params) do
     if offer?(body_params) do
       send_offer(
-        url(body_params["id"]),
+        url(),
         offer_params(body_params)
       )
     end
   end
 
   def offer?(body_params) do
-    Enum.member?(body_params["tags"], "zapatos") && body_params["price"] < 100
+    IO.inspect(body_params, label: "articulo interesante recibido")
+
+    IO.inspect(
+      Enum.member?(body_params["tags"], "zapatos") && body_params["price"] < 100,
+      label: "oferto?"
+    )
   end
 
-  def url(id) do
-    "http://localhost:4000/bids/#{id}/offers"
+  def url() do
+    "http://localhost:4000/bids/offer"
   end
 
   def offer_params(body_params) do
     Poison.encode!(%{
-      price: (body_params["price"] + 10),
-      user_id: 45
+      # change the buyer name
+      buyer: "A",
+      price: body_params["price"] + 10,
+      bid_id: body_params["id"]
     })
   end
 
   def send_offer(url, params) do
-    HTTPoison.post!(url, params, [{"content-type", "application/json"}])
+    IO.inspect(params, label: "voy a enviar estos parametros \n")
+    res = HTTPoison.post!(url, params, [{"content-type", "application/json"}])
+    IO.inspect(res, label: "recibido luego de ofertar")
   end
 end
