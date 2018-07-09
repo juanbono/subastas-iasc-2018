64d343fb482cdc8a203ca187837a53d413227998
Juan Bono <juanbono94@gmail.com>
2018-07-09 16:07:38 -0300

    separate each application

--- apps/exchange/mix.exs
+++ apps/exchange/mix.exs
@@ -5,10 +5,10 @@ defmodule Exchange.MixProject do
     [
       app: :exchange,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
+      build_path: "./_build",
+      config_path: "./config/config.exs",
+      deps_path: "./deps",
+      lockfile: "./mix.lock",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -26,9 +26,13 @@ defmodule Exchange.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true},
+      {:cowboy, "~> 2.4.0"},
+      {:plug, "~> 1.5.1"},
+      {:poison, "~> 3.1"},
+      {:httpoison, "~> 1.2.0"},
+      {:amnesia, "~> 0.2.7"},
+      {:elixir_uuid, "~> 1.2"},
+      {:unsplit, git: "https://github.com/discordapp/unsplit"}
     ]
   end
 end
