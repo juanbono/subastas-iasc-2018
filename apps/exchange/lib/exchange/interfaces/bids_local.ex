c65b78776236feb6ffb767fd46d34630df79636c
juan bono <juanbono94@gmail.com>
2018-07-21 16:55:18 -0300

    add docs

--- apps/exchange/lib/exchange/interfaces/bids_local.ex
+++ apps/exchange/lib/exchange/interfaces/bids_local.ex
@@ -1,7 +1,7 @@
 defmodule Exchange.Interfaces.Bids.Local do
   @moduledoc """
   Modulo con las funciones relativas a las apuestas que son utilizadas
-  por otros modulos.
+  por otros modulos. La implementacion de las mismas utiliza modulos locales.
   """
   defdelegate process(operation, offer_data), to: Exchange.Bids
 
